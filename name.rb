0080d58b6bb8a7f5ec1cec02b0303df8fcbb051f
William Reilly <william.reilly2010@gmail.com>
2014-12-21 03:36:39 +0000

    Added name.rb for Exercise 1 of Variables

---
+++ name.rb
@@ -0,0 +1,7 @@
+#Variables: Exercise #1
+#Write a program that asks the user to type in their name
+#and then prints out a greet message with their name included
+
+puts "What's your name?"
+name = gets.chomp
+puts "Hello, #{name}. \nIt's nice to meet you."
